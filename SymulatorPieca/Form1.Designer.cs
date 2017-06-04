4703a9b6aab2727a837892a9f12baf9ad967fdb6
negocki <katalko@gmail.com>
2017-06-04 17:42:47 +0200

    rozwinięto interfejs, obsługa paleniska, rozpalanie, wygaszanie

--- SymulatorPieca/Form1.Designer.cs
+++ SymulatorPieca/Form1.Designer.cs
@@ -47,8 +47,35 @@
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.button_ruszt_start = new System.Windows.Forms.Button();
             this.button_ruszt_stop = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.trackBar_pompazim = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_awariaruszt = new System.Windows.Forms.Button();
+            this.button_awariapomp = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.button_awaria_went = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label_wodazim = new System.Windows.Forms.Label();
+            this.label_wodaciep = new System.Windows.Forms.Label();
+            this.label_palenisko = new System.Windows.Forms.Label();
+            this.timer_symulacja = new System.Windows.Forms.Timer(this.components);
+            this.timer_awaria = new System.Windows.Forms.Timer(this.components);
+            this.button_rozpal = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_pompazim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -65,7 +92,7 @@
             // label_fan1
             // 
             this.label_fan1.AutoSize = true;
-            this.label_fan1.Location = new System.Drawing.Point(305, 72);
+            this.label_fan1.Location = new System.Drawing.Point(210, 182);
             this.label_fan1.Name = "label_fan1";
             this.label_fan1.Size = new System.Drawing.Size(46, 13);
             this.label_fan1.TabIndex = 1;
@@ -74,7 +101,7 @@
             // label_fan2
             // 
             this.label_fan2.AutoSize = true;
-            this.label_fan2.Location = new System.Drawing.Point(521, 94);
+            this.label_fan2.Location = new System.Drawing.Point(508, 94);
             this.label_fan2.Name = "label_fan2";
             this.label_fan2.Size = new System.Drawing.Size(46, 13);
             this.label_fan2.TabIndex = 2;
@@ -83,20 +110,20 @@
             // label_pompazim
             // 
             this.label_pompazim.AutoSize = true;
-            this.label_pompazim.Location = new System.Drawing.Point(521, 128);
+            this.label_pompazim.Location = new System.Drawing.Point(508, 127);
             this.label_pompazim.Name = "label_pompazim";
-            this.label_pompazim.Size = new System.Drawing.Size(73, 13);
+            this.label_pompazim.Size = new System.Drawing.Size(109, 13);
             this.label_pompazim.TabIndex = 3;
-            this.label_pompazim.Text = "Pompa zimna:";
+            this.label_pompazim.Text = "Pompa zimna: 0 RPM";
             // 
             // label_pompaciep
             // 
             this.label_pompaciep.AutoSize = true;
-            this.label_pompaciep.Location = new System.Drawing.Point(521, 210);
+            this.label_pompaciep.Location = new System.Drawing.Point(508, 210);
             this.label_pompaciep.Name = "label_pompaciep";
-            this.label_pompaciep.Size = new System.Drawing.Size(76, 13);
+            this.label_pompaciep.Size = new System.Drawing.Size(112, 13);
             this.label_pompaciep.TabIndex = 4;
-            this.label_pompaciep.Text = "Pompa ciepła:";
+            this.label_pompaciep.Text = "Pompa ciepła: 0 RPM";
             // 
             // progressBar1
             // 
@@ -157,13 +184,14 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button_awariaruszt);
             this.groupBox1.Controls.Add(this.button_ruszt_stop);
             this.groupBox1.Controls.Add(this.button_ruszt_start);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
             this.groupBox1.Location = new System.Drawing.Point(19, 321);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(188, 81);
+            this.groupBox1.Size = new System.Drawing.Size(188, 111);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Prędkość rusztu";
@@ -212,11 +240,217 @@
             this.button_ruszt_stop.UseVisualStyleBackColor = true;
             this.button_ruszt_stop.Click += new System.EventHandler(this.button_ruszt_stop_Click);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_awariapomp);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Controls.Add(this.trackBar_pompazim);
+            this.groupBox2.Location = new System.Drawing.Point(213, 321);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(188, 111);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pompy";
+            // 
+            // trackBar_pompazim
+            // 
+            this.trackBar_pompazim.LargeChange = 10;
+            this.trackBar_pompazim.Location = new System.Drawing.Point(78, 19);
+            this.trackBar_pompazim.Name = "trackBar_pompazim";
+            this.trackBar_pompazim.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_pompazim.SmallChange = 10;
+            this.trackBar_pompazim.TabIndex = 0;
+            this.trackBar_pompazim.Scroll += new System.EventHandler(this.trackBar_pompazim_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(78, 48);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Zimna";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ciepła";
+            // 
+            // button_awariaruszt
+            // 
+            this.button_awariaruszt.Enabled = false;
+            this.button_awariaruszt.Location = new System.Drawing.Point(42, 82);
+            this.button_awariaruszt.Name = "button_awariaruszt";
+            this.button_awariaruszt.Size = new System.Drawing.Size(108, 23);
+            this.button_awariaruszt.TabIndex = 4;
+            this.button_awariaruszt.Text = "Napraw awarie";
+            this.button_awariaruszt.UseVisualStyleBackColor = true;
+            // 
+            // button_awariapomp
+            // 
+            this.button_awariapomp.Enabled = false;
+            this.button_awariapomp.Location = new System.Drawing.Point(41, 82);
+            this.button_awariapomp.Name = "button_awariapomp";
+            this.button_awariapomp.Size = new System.Drawing.Size(108, 23);
+            this.button_awariapomp.TabIndex = 5;
+            this.button_awariapomp.Text = "Napraw awarie";
+            this.button_awariapomp.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button_awaria_went);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.trackBar3);
+            this.groupBox3.Location = new System.Drawing.Point(407, 321);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(188, 82);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Wentylator #1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Prędkość";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 10;
+            this.trackBar3.Location = new System.Drawing.Point(78, 19);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.SmallChange = 10;
+            this.trackBar3.TabIndex = 0;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // button_awaria_went
+            // 
+            this.button_awaria_went.Enabled = false;
+            this.button_awaria_went.Location = new System.Drawing.Point(39, 51);
+            this.button_awaria_went.Name = "button_awaria_went";
+            this.button_awaria_went.Size = new System.Drawing.Size(108, 23);
+            this.button_awaria_went.TabIndex = 6;
+            this.button_awaria_went.Text = "Napraw awarie";
+            this.button_awaria_went.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label_palenisko);
+            this.groupBox4.Controls.Add(this.label_wodaciep);
+            this.groupBox4.Controls.Add(this.label_wodazim);
+            this.groupBox4.Controls.Add(this.label10);
+            this.groupBox4.Controls.Add(this.label9);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Location = new System.Drawing.Point(655, 321);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(188, 82);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Parametry wyjściowe";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Woda - wejście: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 37);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Woda - wyjście:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 56);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(121, 13);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Palenisko - temperatura:";
+            // 
+            // label_wodazim
+            // 
+            this.label_wodazim.AutoSize = true;
+            this.label_wodazim.Location = new System.Drawing.Point(94, 24);
+            this.label_wodazim.Name = "label_wodazim";
+            this.label_wodazim.Size = new System.Drawing.Size(23, 13);
+            this.label_wodazim.TabIndex = 5;
+            this.label_wodazim.Text = "0 C";
+            // 
+            // label_wodaciep
+            // 
+            this.label_wodaciep.AutoSize = true;
+            this.label_wodaciep.Location = new System.Drawing.Point(94, 37);
+            this.label_wodaciep.Name = "label_wodaciep";
+            this.label_wodaciep.Size = new System.Drawing.Size(23, 13);
+            this.label_wodaciep.TabIndex = 6;
+            this.label_wodaciep.Text = "0 C";
+            // 
+            // label_palenisko
+            // 
+            this.label_palenisko.AutoSize = true;
+            this.label_palenisko.Location = new System.Drawing.Point(133, 56);
+            this.label_palenisko.Name = "label_palenisko";
+            this.label_palenisko.Size = new System.Drawing.Size(23, 13);
+            this.label_palenisko.TabIndex = 7;
+            this.label_palenisko.Text = "0 C";
+            // 
+            // timer_symulacja
+            // 
+            this.timer_symulacja.Enabled = true;
+            this.timer_symulacja.Interval = 1000;
+            this.timer_symulacja.Tick += new System.EventHandler(this.timer_symulacja_Tick);
+            // 
+            // timer_awaria
+            // 
+            this.timer_awaria.Interval = 10000;
+            // 
+            // button_rozpal
+            // 
+            this.button_rozpal.Location = new System.Drawing.Point(407, 409);
+            this.button_rozpal.Name = "button_rozpal";
+            this.button_rozpal.Size = new System.Drawing.Size(188, 23);
+            this.button_rozpal.TabIndex = 15;
+            this.button_rozpal.Text = "Rozpal palenisko";
+            this.button_rozpal.UseVisualStyleBackColor = true;
+            this.button_rozpal.Click += new System.EventHandler(this.button_rozpal_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(866, 468);
+            this.Controls.Add(this.button_rozpal);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -231,9 +465,19 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_pompazim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -258,6 +502,27 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button button_ruszt_stop;
         private System.Windows.Forms.Button button_ruszt_start;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar trackBar_pompazim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button_awariaruszt;
+        private System.Windows.Forms.Button button_awariapomp;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button_awaria_went;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label_palenisko;
+        private System.Windows.Forms.Label label_wodaciep;
+        private System.Windows.Forms.Label label_wodazim;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer_symulacja;
+        private System.Windows.Forms.Timer timer_awaria;
+        private System.Windows.Forms.Button button_rozpal;
     }
 }
 
--- SymulatorPieca/Form1.cs
+++ SymulatorPieca/Form1.cs
@@ -13,7 +13,22 @@ namespace SymulatorPieca
     public partial class Form1 : Form
     {
         public int ruszt = 0;
+        public int fan1_speed = 0;
+        public int pompa_zimna = 0;
+        public int pompa_ciepla = 0;
+        public int woda_zimna = 30;
+        public int woda_ciepla = 80;
+        public int temp_palenisko = 0;
+        public int ruszt_speed = 0;
+
+        public bool rozpalony = false;
+        public bool zalogowany = true;
         public bool ruszt_start = false;
+        public bool awaria_ruszt = false;
+        public bool awaria_pomp = false;
+        public bool awaria_went = false;
+
+        public Random randomizer = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +38,7 @@ namespace SymulatorPieca
         {
             timer_ruszt.Stop();
             timer_ruszt.Interval = 1000;
+            ruszt_speed = 200;
             timer_ruszt.Start();
         }
 
@@ -47,6 +63,7 @@ namespace SymulatorPieca
         {
             timer_ruszt.Stop();
             timer_ruszt.Interval = 2000;
+            ruszt_speed = 100;
             timer_ruszt.Start();
         }
 
@@ -54,12 +71,85 @@ namespace SymulatorPieca
         {
             ruszt_start = true;
             ruszt = 0;
+            if (radioButton1.Checked) ruszt_speed = 200;
+            else ruszt_speed = 100;
             timer_ruszt.Start();
         }
 
         private void button_ruszt_stop_Click(object sender, EventArgs e)
         {
             ruszt_start = false;
+            ruszt_speed = 0;
+        }
+
+        private void trackBar_pompazim_Scroll(object sender, EventArgs e)
+        {
+            label_pompazim.Text = "Pompa zimna: " + (trackBar_pompazim.Value * 100).ToString() + " RPM";
+            pompa_zimna = trackBar_pompazim.Value * 100;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label_pompaciep.Text = "Pompa ciepła: " + (trackBar1.Value * 100).ToString() + " RPM";
+            pompa_ciepla = trackBar1.Value * 100;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            label_fan1.Text = "Fan 1: " + (trackBar3.Value * 100).ToString() + " RPM";
+            fan1_speed = trackBar3.Value * 100;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_symulacja_Tick(object sender, EventArgs e)
+        {
+            if(rozpalony) //czy palenisko zostało rozpalone?
+            {
+                if (ruszt_speed > 0 && fan1_speed > 0) //jezeli wentylator i ruszt pracują to zwiekszamy temperature
+                {
+                    if (temp_palenisko < ruszt_speed + fan1_speed) //po przekroczeniu pewnej temp. nie zwiekszamy juz
+                    {
+                        double ruszt = ruszt_speed * 0.1;
+                        double fans = fan1_speed * 0.1;
+                        temp_palenisko = temp_palenisko + Convert.ToInt32(ruszt) + Convert.ToInt32(fans);
+                    }
+                    else
+                    {
+                        temp_palenisko = ruszt_speed + fan1_speed; //drobne wahania temp. dla realizmu
+                        int znak = randomizer.Next(0, 1);
+                        if (znak == 0) znak = -1;
+                        temp_palenisko += znak * randomizer.Next(0, 100);
+                    }
+                }
+                else
+                {
+                    if (temp_palenisko > 0) 
+                        temp_palenisko = temp_palenisko - (200 - ruszt_speed) - Convert.ToInt32(fan1_speed*0.01) - 100; //wygaszanie paleniska, do poprawki
+                    else
+                    {
+                        temp_palenisko = 0;
+                        rozpalony = false; //palenisko wygasło
+                    }
+                }
+                label_palenisko.Text = temp_palenisko.ToString()+" C";
+            }
+            else
+            {
+                temp_palenisko = 0;
+                label_palenisko.Text = temp_palenisko.ToString() + " C";
+            }
+
+
+        }
+
+        private void button_rozpal_Click(object sender, EventArgs e)
+        {
+            temp_palenisko = 400;
+            rozpalony = true; //rozpalamy
         }
     }
 }
